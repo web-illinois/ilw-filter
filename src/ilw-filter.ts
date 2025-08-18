cf6cc773bd0023eb84aa6a1865bbb88ae424a1a2
Mikael Arvola <marvel@illinois.edu>
2025-08-18 15:49:40 -0500

    Add tests to GitHub Actions.

--- src/ilw-filter.ts
+++ src/ilw-filter.ts
@@ -1,7 +1,5 @@
 import "./Filter";
 import "./items/FilterSearch"
 import "./items/FilterHidden"
-import "./items/FilterMulti"
-import "./items/FilterList"
 import "./items/FilterCheckboxesSimple"
 import "./items/FilterDropdownSimple"
