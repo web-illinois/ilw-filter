12b5483124a5695e08833e9ad82cc11c4ac321cb
Mikael Arvola <marvel@illinois.edu>
2025-09-19 14:46:27 -0500

    Point axe tests to index.html.

--- test-axe/axe.test.ts
+++ test-axe/axe.test.ts
@@ -3,7 +3,7 @@ import { axeTestFunction } from "@illinois-toolkit/ilw-core";
 
 test.describe("homepage", () => {
     test("should not have any automatically detectable accessibility issues", async ({page}, testInfo) => {
-        const result = await axeTestFunction(page, testInfo);
+        const result = await axeTestFunction(page, testInfo, "./samples/index.html");
 
         expect(result).toBeTruthy();
     });
