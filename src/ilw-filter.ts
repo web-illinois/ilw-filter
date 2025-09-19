0df2c0f411d405d54c18e00f3da8c4539bb7e0c7
Mikael Arvola <marvel@illinois.edu>
2025-09-19 14:39:13 -0500

    Export types from the main file.

--- src/ilw-filter.ts
+++ src/ilw-filter.ts
@@ -1,5 +1,13 @@
 import "./Filter";
-import "./items/FilterSearch"
-import "./items/FilterHidden"
-import "./items/FilterCheckboxesSimple"
-import "./items/FilterDropdownSimple"
+import "./items/FilterSearch";
+import "./items/FilterHidden";
+import "./items/FilterCheckboxesSimple";
+import "./items/FilterDropdownSimple";
+
+export * from "./Filter";
+export * from "./FilterContext";
+export * from "./items/FilterCheckboxesSimple";
+export * from "./items/FilterDropdownSimple";
+export * from "./items/FilterHidden";
+export * from "./items/FilterItem";
+export * from "./items/FilterSearch";
